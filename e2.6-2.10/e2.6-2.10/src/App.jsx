ba3f78edfbfeb1ab7b94f5a73376464865788945
rober <alvaro04cg@gmail.com>
2024-07-29 17:56:18 +0200

    E2.8

--- e2.6-2.10/e2.6-2.10/src/App.jsx
+++ e2.6-2.10/e2.6-2.10/src/App.jsx
@@ -5,25 +5,29 @@ const App = () => {
 
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
+      number: '040-1234567',
       id: 1
      }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
   
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     if(checkName(event.target.value) == false) {
-      console.log("No valido")
+      console.log("Not valid name")
       return
     } else {
       setNewName(event.target.value)
@@ -39,6 +43,24 @@ const App = () => {
     return true
   }
 
+  const handleNumberChange = (event) => {
+    if(checkNumber(event.target.value) == false)  {
+      console.log("Not valid number")
+      return
+    } else {
+      setNewNumber(event.target.value)
+    }
+  }
+
+  const checkNumber = (newNumber) => {
+    const numberExists = persons.some(p => p.number === newNumber)
+    if (numberExists) {
+      alert(`${newNumber} is already added to phonebook`)
+      return false 
+    }
+    return true
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -47,6 +69,11 @@ const App = () => {
           name: <input 
                     value={newName}
                     onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input 
+                    value={newNumber}
+                    onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
@@ -58,7 +85,7 @@ const App = () => {
   )
 }
 
-const Persons = ({ persons }) => <>{persons.map((p) => <p key={p.id}>{p.name}</p>)}</>
+const Persons = ({ persons }) => <>{persons.map((p) => <p key={p.id}>{p.name}   {p.number}</p>)}</>
 
 
 export default App
