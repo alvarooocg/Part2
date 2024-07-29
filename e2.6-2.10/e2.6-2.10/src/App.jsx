40e7631a3bf90bde6d8ac5e7d77d6e777b56c5d9
rober <alvaro04cg@gmail.com>
2024-07-29 18:20:59 +0200

    E2.9

--- e2.6-2.10/e2.6-2.10/src/App.jsx
+++ e2.6-2.10/e2.6-2.10/src/App.jsx
@@ -4,13 +4,15 @@ import './App.css'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567',
-      id: 1
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -61,9 +63,21 @@ const App = () => {
     return true
   }
 
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input 
+                                value={filter}
+                                onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input 
@@ -80,7 +94,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={filteredPersons} />
     </div>
   )
 }
