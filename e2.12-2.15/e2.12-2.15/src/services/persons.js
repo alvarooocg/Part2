e72707afb8d4a97c9ffbf067a0fbedd1dca71062
rober <alvaro04cg@gmail.com>
2024-08-04 00:03:13 +0200

    E2.13

--- e2.12-2.15/e2.12-2.15/src/services/persons.js
+++ e2.12-2.15/e2.12-2.15/src/services/persons.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3004/persons'
 
+// THIS CODE WAS JUST IMPLEMENTED THE STEP 2.12
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
